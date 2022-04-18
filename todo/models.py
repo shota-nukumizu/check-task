4e877cd6dbe6a5ab316e3165cedfa23da9b9b163
shota-nukumizu <author2000.1225@gmail.com>
2022-04-18 15:02:37 +0900

    created database

--- todo/models.py
+++ todo/models.py
@@ -1,2 +1,15 @@
 from django.db import models
 
+PRIORITY_PROPERTIES = (
+    ('highlight', 'highlight'),
+    ('middle', 'middle'),
+    ('low', 'low'),
+)
+
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(blank=True)
+    priority = models.CharField(max_length=15, choices=PRIORITY_PROPERTIES)
+    created = models.DateField(auto_now=True)
+    due = models.DateField()
+    done = models.BooleanField(default=False)
